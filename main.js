190e0efa876bf892a7736835941accf39ef99f61
Sean Garrett Loveland <seangloveland@gmail.com>
2022-04-18 20:13:45 -0700

    Added image gallery, updated styles for text around the website

---
+++ main.js
@@ -0,0 +1,27 @@
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+const images = document.querySelector(".gallery")
+
+let currImage = 1
+const maxImage = 3
+
+prev.addEventListener("click", () => {
+    --currImage
+    if(currImage < 1) {
+        currImage = maxImage
+    }
+
+    let imageString = "images/Band" + currImage + ".png"
+    images.setAttribute("src", imageString)
+})
+
+next.addEventListener("click", () => {
+    ++currImage
+    if(currImage > maxImage) {
+        currImage = 1
+    }
+
+    let imageString = "images/Band" + currImage + ".png"
+    images.setAttribute("src", imageString)
+})
+
